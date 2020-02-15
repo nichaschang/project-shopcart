8539f2673149ba580265f316ced1b278d8856a61
nichaschang <nichaschang@gmail.com>
2020-02-15 16:27:45 +0800

    修改SQL語法

--- outlist2.php
+++ outlist2.php
@@ -1,5 +1,11 @@
 <?php
 require_once("./db.inc.php");
+// echo "<pre>";
+// print_r($_POST);
+// echo "</pre>";
+// exit();
+
+//廠商端 訂單成立
 
 $sql_updateOut="UPDATE `orderlist` SET `outStatus`='已出貨' WHERE `orderId`=?";
 $arr_updateOut=[
@@ -7,5 +13,13 @@ $arr_updateOut=[
 ];
 $stmt=$pdo->prepare($sql_updateOut);
 $stmt->execute($arr_updateOut);
+
+
+$sql_updateOutDetail="UPDATE `orderdetail` SET `outStatus`='已出貨' WHERE `orderId`=?";
+$arr_updateOutDetail=[
+    $_POST['orderId']
+];
+$stmt=$pdo->prepare($sql_updateOutDetail);
+$stmt->execute($arr_updateOutDetail);
 
 ?>
