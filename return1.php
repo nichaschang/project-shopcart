442903b0b8a196e844cc7912563212b28d1b902c
nichaschang <nichaschang@gmail.com>
2020-02-15 16:30:22 +0800

    修改SQL語法

--- return1.php
+++ return1.php
@@ -1,52 +1,35 @@
 <?php
 require_once("./db.inc.php");
 
-// echo "<pre>";
-// print_r($_POST['pIdarr'][0]);
-// print_r($_POST['pIdarr'][1]);
-// echo "</pre>";
-// exit();
-$sql_returnlist="INSERT INTO `returnlist`(`orderId`,`returnPay`,`csPhone`,`csAdress`,`returnReason`) VALUES (?,?,?,?,?)";
+//退貨單資料插入
+
+$sql_returnlist="INSERT INTO `returnlist`(`orderId`,`returnStatus`,`returnPay`,`buyerName`,`buyerPhone`,`buyerAdress`,`returnReason`) VALUES (?,'退貨處理中',?,?,?,?,?)";
 $arr_returnlist=[
     $_POST['orderId'],
-    $_POST['reMoney'],
-    $_POST['csPhone'],
-    $_POST['csAdress'],
+    $_POST['returnPay'],
+    $_POST['buyerName'],
+    $_POST['buyerPhone'],
+    $_POST['buyerAdress'],
     $_POST['returnMsg']
 ];
 $stmt=$pdo->prepare($sql_returnlist);
 $stmt->execute($arr_returnlist);
-// echo "<pre>";
-// print_r($_POST['pIdarr']);
-// echo "</pre>";
-// exit();
-$returnlistId = $pdo->lastInsertId();
+$returnId = $pdo->lastInsertId();
 
-$sql_returndetail="INSERT INTO `returndetail` (`returnlistId`,`pId`,`count`) VALUES (?,?,?)";
+$sql_returndetail="INSERT INTO `returndetail` (`returnId`,`pId`,`count`) VALUES (?,?,?)";
 
-for($i=0,$k=0;$i<count($_POST['pIdarr']);$i++){
+$stmt=$pdo->prepare($sql_returndetail);
+for($i=0;$i<count($_POST['pIdarr']);$i++){
     $arr_returndetail=[
-        $returnlistId,
+        $returnId,
         $_POST['pIdarr'][$i],
         $_POST['countarr'][$i]
     ];
-    $stmt=$pdo->prepare($sql_returndetail);
     $stmt->execute($arr_returndetail);
-    $k++;
-//     echo "<pre>";
-// print_r($arr_returndetail);
-// echo "</pre>";
-// exit();
 };
 
-$sql_updateStatus="UPDATE `orderlist` SET `outStatus`='退貨申請中' WHERE `orderId`=?";
-$arr_updateStatus=[
-    $_POST['orderId']
-];
-$stmt=$pdo->prepare($sql_updateStatus);
-$stmt->execute($arr_updateStatus);
-
-$sql_updateStatus="UPDATE `orderdetail` SET `outStatus`='退貨申請中' WHERE `orderId`=? AND `pId`=?";
+//將原訂單的狀態做更改
+$sql_updateStatus="UPDATE `orderdetail` SET `outStatus`='退貨處理中' WHERE `orderId`=? AND `pId`=?";
 $stmt=$pdo->prepare($sql_updateStatus);
 for($i=0;$i<count($_POST['pIdarr']);$i++){
 $arr_updateStatus=[
@@ -55,4 +38,5 @@ $arr_updateStatus=[
 ];
 $stmt->execute($arr_updateStatus);
 };
+
 ?>
